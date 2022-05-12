977910883a4a90b8326e2435d5278873949eeba3
Vitaliy <nezhinskiy22@gmail.com>
2022-05-12 16:12:21 +0300

    sorting added

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,21 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import TableRow from "./TableRow";
 
 const List = ({ data }) => {
+  const [rowData, setRowData] = useState([...data]);
+  const [order, setOrder] = useState("start");
+
+  const sort = (col) => {
+    if (order === "start") {
+      const sorted = [...rowData].sort((a, b) =>
+        a[col].toString() > b[col].toString() ? 1 : -1
+      );
+      setRowData(sorted);
+      setOrder("end");
+    }
+    if (order === "end") {
+      const sorted = [...rowData].sort((a, b) =>
+        a[col].toString() < b[col].toString() ? 1 : -1
+      );
+      setRowData(sorted);
+      setOrder("start");
+    }
+  };
+
   return (
     <div>
       <table border="1" cellPadding="5" cellSpacing="5">
         <thead>
           <tr>
-            <th>Session</th>
-            <th>Agents on call</th>
-            <th>Started at</th>
-            <th>Finished at</th>
-            <th>Amount of messages</th>
+            <th>
+              <button
+                style={{ padding: "5", width: "80%", fontSize: "16px" }}
+                type="button"
+                onClick={() => sort("session")}
+              >
+                Session
+              </button>
+            </th>
+            <th>
+              <button
+                style={{ padding: "5", width: "80%", fontSize: "16px" }}
+                type="button"
+                onClick={() => sort("agentsOnCall")}
+              >
+                Agents on call
+              </button>
+            </th>
+            <th>
+              <button
+                style={{ padding: "5", width: "80%", fontSize: "16px" }}
+                type="button"
+                onClick={() => sort("startedAt")}
+              >
+                Started at
+              </button>
+            </th>
+            <th>
+              <button
+                style={{ padding: "5", width: "80%", fontSize: "16px" }}
+                type="button"
+                onClick={() => sort("finishedAt")}
+              >
+                Finished at
+              </button>
+            </th>
+            <th>
+              <button
+                style={{ padding: "5", width: "80%", fontSize: "16px" }}
+                type="button"
+                onClick={() => sort("messages")}
+              >
+                Amount of messages
+              </button>
+            </th>
             <th>Latest page</th>
           </tr>
         </thead>
-        {data.slice(0, 20).map((item) => (
+        {rowData.slice(0, 20).map((item) => (
           <TableRow data={item} key={item._id} />
         ))}
       </table>
